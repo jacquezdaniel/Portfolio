100b4ac725c1954c231e2d9979a03b30f972a1db
jacquezdaniel <jacquez.j.daniel@gmail.com>
2018-12-10 00:30:44 -0800

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1200, function(){
+   
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $("section")
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i]
+  
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible")
+      } else {
+        $(tag).removeClass("visible")
+      }
+    }
+  })
