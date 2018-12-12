d6be7d490e0798256e786569b2982d4d1a575285
jacquezdaniel <jacquez.j.daniel@gmail.com>
2018-12-11 23:56:40 -0800

    more updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,18 +17,35 @@ $(document).ready(function() {
   });
 });
 
-$(document).on("scroll", function () {
-    var pageTop = $(document).scrollTop()
-    var pageBottom = pageTop + $(window).height()
-    var tags = $("section")
-  
-    for (var i = 0; i < tags.length; i++) {
-      var tag = tags[i]
-  
-      if ($(tag).position().top < pageBottom) {
-        $(tag).addClass("visible")
-      } else {
-        $(tag).removeClass("visible")
-      }
-    }
-  })
+$(window).on("load",function() {
+  function fade() {
+      var animation_height = $(window).innerHeight() * 0.25;
+      var ratio = Math.round( (1 / animation_height) * 10000 ) / 10000;
+
+      $('section').each(function() {
+
+          var objectTop = $(this).offset().top;
+          var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+          if ( objectTop < windowBottom ) {
+              if ( objectTop < windowBottom - animation_height ) {
+                  $(this).css( {
+                      transition: 'opacity 2s linear',
+                      opacity: 1
+                  } );
+
+              } else {
+                  $(this).css( {
+                      transition: 'opacity 2s linear',
+                      opacity: (windowBottom - objectTop) * ratio
+                  } );
+              }
+          } else {
+              $(this).css( 'opacity', 0 );
+          }
+      });
+  }
+  $('.fade').css( 'opacity', 0 );
+  fade();
+  $(window).scroll(function() {fade();});
+});
